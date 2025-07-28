5210261ac84911fdc8861602a7424f6dfbf45ce7
amiskiewiczpl <amiskiewiczpl@gmail.com>
2025-07-29 01:05:36 +0200

    h

--- src/helpers/loadGoogleMaps.js
+++ src/helpers/loadGoogleMaps.js
@@ -1,7 +1,13 @@
+// helpers/loadGoogleMaps.js
+let loaded = false;
+
 export function loadGoogleMaps() {
+  if (loaded) return Promise.resolve();
+
   return new Promise((resolve, reject) => {
-    if (window.google && window.google.maps) {
-      resolve(window.google);
+    if (typeof window.google === "object" && typeof window.google.maps === "object") {
+      loaded = true;
+      resolve();
       return;
     }
 
@@ -9,8 +15,14 @@ export function loadGoogleMaps() {
     script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
     script.async = true;
     script.defer = true;
-    script.onload = () => resolve(window.google);
+
+    script.onload = () => {
+      loaded = true;
+      resolve();
+    };
+
     script.onerror = (err) => reject(err);
+
     document.head.appendChild(script);
   });
 }
