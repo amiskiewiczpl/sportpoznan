9efd6bf1d751dab3bab22c92ac596e9001267925
amiskiewiczpl <amiskiewiczpl@gmail.com>
2025-07-29 01:27:34 +0200

    g

--- src/helpers/loadGoogleMaps.js
+++ src/helpers/loadGoogleMaps.js
@@ -9,8 +9,9 @@ export const loadGoogleMaps = () => {
       }
 
       const script = document.createElement('script');
-          script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
       script.async = true;
+      script.defer = true;
       script.onload = () => resolve(window.google);
       script.onerror = (err) => reject(err);
       document.head.appendChild(script);
